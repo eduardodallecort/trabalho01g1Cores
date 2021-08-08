73ddea6f1530a61e9da1dfd488b59e3416104d66
eduardodallecort <dallecorteduardo@gmail.com>
2021-08-08 20:16:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+        listColors: ['black', 'yellow', 'blue', 'green', 'red'],
+        actualColor: 'green'
+    }
+  }
+
+  drawColor = () => {
+    let pos = Math.floor(Math.random() * this.state.listColors.length)
+    this.setState(currentState => ({
+      actualColor: currentState.listColors[pos]
+    }))
+  }
+
+  render() {
+    let contador = 0
+    while (contador < 5) {
+      setInterval(() => {
+        this.drawColor()
+      }, 1000);
+      contador += 1
+    
+      return (
+        <>
+          <body class="body">
+            <div class="square">
+                <div class="block" style={{background: this.state.actualColor}}>      
+                </div>
+            </div>
+            {
+               <p>Esta cor é: {this.state.actualColor}</p>
+             } 
+          </body>
+        </>
+      )
+    }
+  }
 }
 
 export default App;
